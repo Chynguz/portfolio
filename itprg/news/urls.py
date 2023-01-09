52e558b0c34a6f85efd1d9c6172c993b69b65ec2
Chyngyz <122264868+Chynguz@users.noreply.github.com>
2023-01-09 16:53:26 +0600

    Delete itprg directory

--- itprg/news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.news_home, name = 'news_home'),
-    
-]
--- itprg/news/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def news_home(request):
-    return render(request, 'news/news.html')
